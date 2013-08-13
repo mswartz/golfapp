be739176c4961276b68c10c7b34158fa89c2dc1e
Michael Swartz <mswartz@gmail.com>
2013-08-12 23:13:03 -0400

    added multiselect for courses, started hooking up games to players. Want to add routing but couldnt get Backbone working

--- golf.js
+++ golf.js
@@ -67,12 +67,11 @@ Template.courses.events({
     thisTee.remove();
   },
   'click input.newcourse_submitcourse' : function() {
-
     var tee_count = $('.newcourse_tee').length;
     var hole_count = $('.newcourse_tee').find('.hole_par').length;
     var newcourse = gatherValues('newcourse',['name']);
-
     var tees = [];
+
     for (var x = 0; x < tee_count; x++) {
       var holes = [];
       for (var i = 0; i < hole_count; i++ ) {
@@ -98,9 +97,9 @@ Template.courses.events({
 });
 
 
-//
+// ************
 // All Games view
-//
+// ************
   Template.allgames.helpers({
     games : function(){
       var raw_games = Games.find().fetch();
@@ -127,68 +126,71 @@ Template.courses.events({
   });
 
 
-//
+//************
 // New Game
-//
+//************
 
   Template.newgame.helpers ({
     debug : function(stuff){
       console.log(stuff);
     },
-    course : function(){
-      // return Courses.find({}).fetch()[0];
-      var course = Session.get("course_selected");
+    course_selected : function(){
+      var course = Courses.findOne({"name":Session.get("course_selected")});
       return course;
+    },
+    courses : function(){
+      return Courses.find({}).fetch();
     }
   });
-
-//Set a session var when the newgame is created. 
-  Template.newgame.created = function(){
-    // console.log("created");
-    var course = Courses.findOne();
-    Session.set("course_selected", course);
-  }
 
 //Receiving a score form the card
   Template.newgame.events({
     'click input.submit' : function() {
       var net_score = {};
       var total = 0;
+      var course = Session.get("course_selected");
+      var players = [];
+
+      //suck in the scores from the input table
       for(var i=1; i<19; i++){
         net_score[i] = parseInt($('#score_'+i).val(), 10);
         console.log(net_score[i]);
         total = total + net_score[i];
       }
-      var course = Session.get("course_selected");
-      var players = [];
 
+      //add the scores and data about the game to the players array
       for(var i=0; i<$(".game_player").length; i++){
         players[i] = {
           "player_id" : $(".game_player").data("id"),
+          "course" : course,
           "score" : net_score,
           "total" : total 
         }
       }
 
-      console.log(players);
-
+      //add thoe whole game to the games collection
       Games.insert({
         "course" : course._id,
         "players" : players
       });
+
+      //iterate the players and add their scores to their games table
+      for(var i=0; i<players.length; i++){
+        Players.update(players[i].player_id, {$set : {"games" : players[i]} } );
+      }
     },
 
-    'click input.course_select' : function() {
-      var course = Courses.findOne();
-      Session.set("course_selected", course);
+    //Update the selected course using the select option
+    'change select.course_select' : function() {
+      Session.set("course_selected", $("#course_select").val());
       console.log(Session.get("course_selected"));
     }
   })
 
 
-//
-// Mode Selector
-//
+//*****************
+// Mode Selector : replace with real router some day
+//*****************
 
   Template.mode.helpers({
     mode_selected : function(){

5fe031424651d5867bf0320df1c0116ebfa4a3cc
Michael Swartz <mswartz@gmail.com>
2013-08-20 22:43:56 -0400

    finished restructuring new courseinput method

--- courses.js
+++ courses.js
@@ -19,43 +19,11 @@ Template.courses.helpers({
 });
 
 Template.courses.events({
-  'click input.newcourse_newtee' : function(){
-    var firstTee = $('#newcourse_tee_0').clone();
-    $('#newcourse').append(firstTee);
-  },
-
   'click input.newcourse_removetee' : function(e){
     var thisTee = $(e.target).closest('.newcourse_tee');
     thisTee.remove();
   },
 
-  'click input.newcourse_submitcourse' : function() {
-    var tee_count = $('.newcourse_tee').length;
-    var hole_count = $('.newcourse_tee').find('.hole_par').length;
-    var newcourse = gatherValues('newcourse',['name']);
-    var tees = [];
-
-    for (var x = 0; x < tee_count; x++) {
-      var holes = [];
-      for (var i = 0; i < hole_count; i++ ) {
-        var hole = gatherValues('newcourse_tee_'+x+'_hole_'+i, ['par', 'yards', 'handicap']);
-        holes.push(hole);
-      }
-      var tee = gatherValues('newcourse_tee_'+x, ["color", "par", "yards"]);
-      var in_data = gatherValues('newcourse_tee_'+x+'_in',['yards', 'par']);
-      var out_data = gatherValues('newcourse_tee_'+x+'_out',['yards', 'par']);
-      var tot_data = gatherValues('newcourse_tee_'+x+'_tot',['yards', 'par']);
-      tee['holes'] = holes;
-      tee['in'] = in_data;
-      tee['out'] = out_data;
-      tee['tot'] = tot_data;
-      tees.push(tee);
-    }
-
-    newcourse['tees'] = tees;
-    console.log('Course', newcourse);
-    Courses.insert(newcourse);
-  },
   'click input.delete' : function(){
     Courses.remove(this._id);
   }
